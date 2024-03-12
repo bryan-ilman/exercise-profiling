ec9d2b67fcae2d3fe93bffbd46b277c6a2d1ab4c
bryan-2002 <bryan.raihan@ui.ac.id>
2024-03-12 07:23:00 +0700

    [REFACTORING] Optimizing findStudentWithHighestGpa method

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -47,16 +47,8 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        return studentRepository.findAll().stream()
+                .max(Comparator.comparing(Student::getGpa));
     }
 
     public String joinStudentNames() {
